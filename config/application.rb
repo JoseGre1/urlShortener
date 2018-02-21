d6efaea5eb44c44d3e8d14ec15a2abf96be96712
Jose Hernandez <jhernandez@nativapps.co>
2018-02-21 03:35:13 -0500

    Rails configurations

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
@@ -27,5 +27,7 @@ module UrlShortener
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  get "/:id", controller: "api/v1/urls", action: "redirect"
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :urls, only: %i[create show]
+    end
+  end
+  get "*unmatched_route" => redirect("/404.html")
 end
